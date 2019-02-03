6665b44757b4fa7c7cf0c90299f60b7a3d49d5d3
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-03 14:10:36 -0300

    Don't needed app.configure

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => console.log('Listening on port 3000'));
 
-app.configure('production', () => {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`);
-      else
-        next();
-    });
-  });
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`);
+  else
+    next();
+});
