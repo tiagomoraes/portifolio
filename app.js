17c4d6def5a07c7f2ed61be496a51bf6a26c2036
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-03 14:16:58 -0300

    Added the if for production

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => console.log('Listening on port 3000'));
 
-app.use((req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https')
-    res.redirect(`https://${req.header('host')}${req.url}`);
-  else
-    next();
-});
+let https_redirect = function(req, res, next) {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+};
+
+app.use(https_redirect);
