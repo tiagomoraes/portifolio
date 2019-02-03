bcff63a2abec9e26d4268332bc13b79e26863bb4
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-03 14:07:40 -0300

    Added https redirect via express

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let express = require('express');
-let secure = require('ssl-express-www');
 let app = express();
 
 let port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-app.use(secure)
 
 app.listen(port, () => console.log('Listening on port 3000'));
 
+app.configure('production', () => {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`);
+      else
+        next();
+    });
+  });
