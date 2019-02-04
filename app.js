2e937fd6b22553699332fa3206bddabe27eaedad
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-04 00:55:54 -0300

    Removed pug library

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 let express = require('express');
+let http = require('http');
 let enforce = require('express-sslify');
 
 let port = process.env.PORT || 3000;
 let app = express();
 
-app.set('view engine', 'pug');
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
 app.use(express.static(__dirname + '/public'));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+http.createServer(app).listen(port, function() {
+    console.log('Express server listening on port ' + port);
+});
