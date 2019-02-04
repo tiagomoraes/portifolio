357cab0d07c45d309dd1498d7e47f748cb4c0cef
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-04 00:46:35 -0300

    Added pug library

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let express = require('express');
-var http = require('http');
 let enforce = require('express-sslify');
 
 let port = process.env.PORT || 3000;
 let app = express();
 
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
 app.use(express.static(__dirname + '/public'));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-http.createServer(app).listen(port, function() {
-    console.log('Express server listening on port ' + port);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
