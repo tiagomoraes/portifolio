0a650871c2eb65ef47fe0a9f8e16aa047d2994d7
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-05 03:25:29 -0300

    Changed order

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var enforce = require('express-sslify');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(serveStatic('public', {'index': 'index.html'}));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.use(serveStatic('public', {'index': 'index.html'}));
 
 http.createServer(app).listen(port, function() {
     console.log('Express server listening on port ' + port);
