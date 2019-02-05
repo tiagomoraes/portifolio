634ab2f786881bfc2943a8f5bdfb44ee15157ffb
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-05 03:45:30 -0300

    removed library and added the function to code

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-var express = require('express');
-var http = require('http');
-var serveStatic = require('serve-static');
-var enforce = require('express-sslify');
+let express = require('express');
+let serveStatic = require('serve-static');
 
-var port = process.env.PORT || 3000;
-var app = express();
+let port = process.env.PORT || 3000;
+let app = express();
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(secure);
 
 app.use(serveStatic('public', {'index': 'index.html'}));
 
-http.createServer(app).listen(port, function() {
-    console.log('Express server listening on port ' + port);
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
 });
+
+// Functions
+let secure = (req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers.host === 'tiagomoraes.herokuapp.com') {
+            return res.redirect(301, 'https://www.tiagomoraes.me');
+        } else if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect('https://' + req.headers.host + req.url);
+        } else {
+            return next();
+        }
+    } else {
+        return next();
+    }
+}
