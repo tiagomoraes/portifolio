5f145ac2324ce0868c73668388ab1f186539525c
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-05 03:53:45 -0300

    Trned back to library

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 let express = require('express');
 let serveStatic = require('serve-static');
+let sslRedirect = require('heroku-ssl-redirect');
 
 let port = process.env.PORT || 3000;
 let app = express();
 
-app.use(secure);
+app.use(sslRedirect()); // ensures we are on https on 'production' (default)
 
-app.use(serveStatic('public', {'index': 'index.html'}));
+app.use(serveStatic('public', {'index': 'index.html'})); // serves stati files under public
 
 app.listen(port, function() {
     console.log('Listening on port ' + port);
 });
-
-// Functions
-let secure = (req, res, next) => {
-    if (process.env.NODE_ENV === 'production') {
-        if (req.headers.host === 'tiagomoraes.herokuapp.com') {
-            return res.redirect(301, 'https://www.tiagomoraes.me');
-        } else if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect('https://' + req.headers.host + req.url);
-        } else {
-            return next();
-        }
-    } else {
-        return next();
-    }
-}
