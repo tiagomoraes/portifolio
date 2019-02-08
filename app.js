6eb45e58519b6271004de45bebcc67b48516ce5d
Tiago Moraes <tiago.moraes@citi.org.br>
2019-02-08 04:00:21 -0300

    Added handlebars and nodemailer

--- app.js
+++ app.js
@@ -1,14 +1,83 @@
-let express = require('express');
-let serveStatic = require('serve-static');
-let sslRedirect = require('heroku-ssl-redirect');
+const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const sslRedirect = require('heroku-ssl-redirect');
+const nodemailer = require('nodemailer');
+const EmailTemplate = require('email-templates').EmailTemplate;
 
-let port = process.env.PORT || 3000;
-let app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(sslRedirect()); // ensures we are on https on 'production' (default)
 
-app.use(serveStatic('public', {'index': 'index.html'})); // serves stati files under public
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.listen(port, function() {
+app.use('/public', express.static(path.join(__dirname, 'public'))); // serves stati files under public
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.post('/send', (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Phone: ${req.body.phone}</li>
+        <li>How find me: ${req.body.how_select}</li>
+        <li>Subject: ${req.body.subject}</li>
+    </ul>
+    <h3>Message:</h3>
+    <p>${req.body.message}</p>
+    `;
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main(){
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'contact.tiagomoraes@gmail.com',
+            pass: 'Tbm-3902'
+        }
+        });
+    
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Tiago" <contact.tiagomoraes@gmail.com>', // sender address
+            to: "tiago3902@gmail.com", // list of receivers
+            subject: `[SITE-CONTACT] ${req.body.subject}`, // Subject line
+            html: output // html body
+        };
+    
+        // send mail with defined transport object
+        let info = await transporter.sendMail(mailOptions, (error, info) => {
+            res.render('index', {status:'Massage sent!'});
+        });
+    
+        console.log("Message sent: %s", info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    
+    }
+    
+    main().catch(console.error);
+});
+
+app.listen(port, () => {
     console.log('Listening on port ' + port);
 });
+
+// Funcions
+let sendEmail = () => {
+}
